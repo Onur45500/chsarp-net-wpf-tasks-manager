42fb073ea64aa9d7935b89aaa06bff048ea0066e
Onur45500 <onur.akmese@viacesi.fr>
2024-10-14 16:34:32 +0200

    Initialized the project

---
+++ chsarp-net-wpf-tasks-manager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace chsarp_net_wpf_tasks_manager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
