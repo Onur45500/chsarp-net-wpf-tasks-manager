4e21b06fbdf2740aa5a2a30c1ef1163d219b45a1
Onur45500 <onur.akmese@viacesi.fr>
2024-10-14 16:51:56 +0200

    Fixed an issue with the main window

--- chsarp-net-wpf-tasks-manager/Views/MainWindow.xaml.cs
+++ chsarp-net-wpf-tasks-manager/Views/MainWindow.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace chsarp_net_wpf_tasks_manager
+
+namespace chsarp_net_wpf_tasks_manager.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
