41cd6f0941e0b1668b801e463dbb9db1729ef345
Onur45500 <onur.akmese@viacesi.fr>
2024-10-15 09:35:29 +0200

    Added the dictionnaries and the model

---
+++ chsarp-net-wpf-tasks-manager/Models/Task.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chsarp_net_wpf_tasks_manager.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsComplete { get; set; }
+        public TimeSpan Timer { get; set; }
+    }
+
+    public enum TaskState
+    {
+        InProgress,
+        Complete,
+        NotStarted,
+        Late,
+        Archived,
+        Deleted
+    }
+
+    public enum TaskCategory
+    {
+        Work,
+        Personal,
+        Home,
+        HealthWelBeing,
+        Finance,
+        Shopping,
+        SocialFamily,
+        Education,
+        Travel,
+        Errands,
+        HobbiesLeisure,
+        VolunteeringCommunity,
+        BirthdaysAnniversaries,
+        Projects,
+        LongTermGoals
+    }
+
+    public enum TaskImportance
+    {
+        Low,
+        Medium,
+        High,
+        Critical
+    }
+}
